003ae5e1729a34d9b3787cdd1da83570b41b67af
Gemeng Vang <gemengvang96@gmail.com>
2022-03-11 16:24:06 -0600

    Implemented choices and completed Exit prompt

--- Program.cs
+++ Program.cs
@@ -6,7 +6,39 @@ namespace AssessmentCapsuleHotel
     {
         public static void Main()
         {
-            SelectFromMenu();
+            string[] capsules = new string[100];
+            bool menuRunning = true;
+
+            while (menuRunning)
+            {
+                switch (SelectFromMenu())
+                {
+                    case "1":
+                        //Check in guest
+                        break;
+                    case "2":
+                        //check out guess
+                        break;
+                    case "3":
+                        //view guests 10 capsules, 5 from above, 5 from below
+                        break;
+                    case "4":
+                        //Exit
+                        Console.WriteLine(@"====
+Are you sure you want to exit?
+All data will be lost.
+Exit [y/n]: ");
+                        
+                        if (Exit(Console.ReadLine().ToLower()) == "y")
+                        {
+                            Console.WriteLine("Goodbye!");
+                            menuRunning = false;
+                        }
+                        break;
+
+                    
+                }
+            }
 
         }
 
@@ -27,6 +59,32 @@ Choose on option [1-4]:");
 
             return Console.ReadLine();
         }
+
+        private static string Exit(string input)
+        {
+            bool stayOnMenu = true;
+            while (stayOnMenu)
+            {
+                switch (input)
+                {
+                    case "n":
+                        return input;
+                        break;
+                    case "y":
+                        
+                        stayOnMenu = false;
+                        return input;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid entry, please enter in y or n.");
+                        input = Console.ReadLine();
+                        break;
+                }
+
+            }
+            return input;
+        }
+
     }
 
 }
