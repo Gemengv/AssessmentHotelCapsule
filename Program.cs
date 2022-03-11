98b6706f5ab0bf6509b5865b61758bc980884001
Gemeng Vang <gemengvang96@gmail.com>
2022-03-11 15:55:52 -0600

    Implemented display and selecting from main menu

--- Program.cs
+++ Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace AssessmentCapsuleHotel
+{
+    class MainClass
+    {
+        public static void Main()
+        {
+            SelectFromMenu();
+
+        }
+
+        private static string SelectFromMenu()
+        {
+            Console.WriteLine(@"Welcome to Capsule-Capsule.
+===========================
+Enter the number of capsules available: 100
+
+There are 100 unoccupied capsules ready to be booked.");
+            Console.Write(@"Guest Menu
+==========
+1. Check In
+2. Check Out
+3. View Guests
+4. Exit
+Choose on option [1-4]:");
+
+            return Console.ReadLine();
+        }
+    }
+
+}
